224c1a27bf5a97f1596fd19f9e9996128de348d6
wnsdk <jn307742@gmail.com>
2024-06-08 13:25:53 +0900

    v2. Spring Security 적용하기

---
+++ backend/user/src/main/java/com/example/user/security/CustomUserDetails.java
@@ -0,0 +1,28 @@
+package com.example.user.security;
+
+import com.example.user.global.model.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public record CustomUserDetails(User user) implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return "{noop}";
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+}
---
+++ backend/user/src/main/java/com/example/user/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.user.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/admin").hasAuthority("ADMIN")
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
